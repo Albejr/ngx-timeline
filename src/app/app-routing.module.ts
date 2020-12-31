6ff28ee9164b7d90506820f95d0068a7a6c001f9
7amou3 <993610+7amou3@users.noreply.github.com>
2020-12-31 02:33:20 +0100

    remove routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,3 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { registerLocaleData } from '@angular/common';
-import portugues from '@angular/common/locales/pt';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgxTimelineAlbeModule } from 'ngx-timeline-albe';
-
-registerLocaleData(portugues, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -17,11 +14,11 @@ registerLocaleData(portugues, 'pt-BR');
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     NgxTimelineAlbeModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
