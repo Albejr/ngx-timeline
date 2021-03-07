736f39cab67d73c1ea145a69552a91fb10871b73
albejr <jr.albertino@outlook.com>
2021-03-07 20:24:29 -0300

    Add forChild

--- projects/ngx-timeline-albe/src/lib/ngx-timeline-albe.module.ts
+++ projects/ngx-timeline-albe/src/lib/ngx-timeline-albe.module.ts
@@ -1,27 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
-import { NgxTimelineAlbeComponent } from './ngx-timeline-albe.component';
-import { ReplaceTagDirective } from './tag-replace.directive';
-import { SafeHtmlPipe } from './safe-html.pipe';
-
-export const routerModule = RouterModule.forRoot([], {
-  scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled',
-  scrollOffset: [0, 50] // [x, y]
-});
+import { NgxTimelineAlbeComponent } from "./ngx-timeline-albe.component";
+import { ReplaceTagDirective } from "./tag-replace.directive";
+import { SafeHtmlPipe } from "./safe-html.pipe";
 
 @NgModule({
-  declarations: [
-    NgxTimelineAlbeComponent,
-    ReplaceTagDirective,
-    SafeHtmlPipe
-  ],
+  declarations: [NgxTimelineAlbeComponent, ReplaceTagDirective, SafeHtmlPipe],
   imports: [
-    CommonModule,
-    routerModule
+    CommonModule
   ],
-  exports: [NgxTimelineAlbeComponent]
+  exports: [NgxTimelineAlbeComponent],
 })
-export class NgxTimelineAlbeModule { }
+export class NgxTimelineAlbeModule {
+  static forRoot(): ModuleWithProviders<NgxTimelineAlbeModule> {
+    return {
+      ngModule: NgxTimelineAlbeModule,
+      providers: [],
+    };
+  }
+  static forChild(): ModuleWithProviders<NgxTimelineAlbeModule> {
+    return {
+      ngModule: NgxTimelineAlbeModule,
+      providers: [],
+    };
+  }
+}
